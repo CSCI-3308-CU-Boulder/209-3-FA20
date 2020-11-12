432695dd6c5002148ad5df65c473bffb1471769a
MattEMoran <matt.moran23@yahoo.com>
2020-11-11 20:07:31 -0700

    Enemy movement

--- Project Components/Godot_Game_Folder/Enemy.gd
+++ Project Components/Godot_Game_Folder/Enemy.gd
@@ -1,8 +1,15 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
+var currNum = 0
+
 	
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		queue_free()
 	pass 
+	
+func _process(delta):
+	currNum = currNum + 0.01
+	set_position(get_position() + Vector2((cos(currNum))/3,0))	
+	pass
