5684c44eb6d088d8a0549ea53ad42598b314715f
Sam Falberg <samuelfalberg@gmail.com>
2020-11-24 16:01:24 -0700

    Format pause and level complete menus

--- Project Components/Godot_Game_Folder/GameComplete.gd
+++ Project Components/Godot_Game_Folder/GameComplete.gd
@@ -15,13 +15,8 @@ func get_time():
 
 func _setupPopupMenu():
 	#self.add_item("You completed the level in " + str(stepify(get_time(),.01), 0) + " seconds!")
-	get_child(1).get_child(0).get_child(0).text = "The COVID-19 cure teleported to another world! Go find it!\nLevel 1 Time: " + str(stepify(get_time(),.01), 0) + " seconds"
+	get_child(1).get_child(0).get_child(0).text = "You found the cure! 2020 is saved!\nLevel 2 Time: " + str(stepify(get_time(),.01), 0) + " seconds"
 	self.show()
-
-# Go to next level and unpause game
-func _on_NextLevelButton_pressed():
-	get_tree().change_scene("res://world2.tscn")
-	get_tree().paused = not get_tree().paused 
 
 # Go back to title screen and unpause game
 func _on_ToTitleButton_pressed():
--- Project Components/Godot_Game_Folder/Pause.gd
+++ Project Components/Godot_Game_Folder/Pause.gd
@@ -5,3 +5,14 @@ func _input(event):
 		var new_pause_state = not get_tree().paused 
 		get_tree().paused = new_pause_state
 		visible = new_pause_state # Menu is only visible when paused
+
+# Unpause game on resume button press
+func _on_ResumeButton_pressed():
+	var new_pause_state = not get_tree().paused 
+	get_tree().paused = new_pause_state
+	visible = new_pause_state # Menu is only visible when paused
+
+# Return to menu and unpause game on button press
+func _on_QuitButton_pressed():
+	get_tree().change_scene("res://title_screen/TitleScreen.tscn")
+	get_tree().paused = not get_tree().paused 
